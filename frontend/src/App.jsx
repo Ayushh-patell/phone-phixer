1955a8e84d7e668999a7e0671223da90494d2cb6
mastermindsjunior <admin@mastermindsjunior.com>
2025-12-22 18:21:46 +0530

    minor change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router'
 const App = () => {
   return (
     <div>
-      <Link to={'/dashboard'}>
+
+    To go to the dashboard page click the link{" "}
+      <Link className=' text-blue-400 hover:text-blue-700 cursor-pointer' to={'/dashboard'}>
         Dashboard
       </Link>
     </div>

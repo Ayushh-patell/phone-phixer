b62b67c462fc29f8e6d1a8ac629e014fe3f7b36f
Ayushh-patell <ayush.patel.code@gmail.com>
2026-02-24 14:57:40 +0530

    proper email

--- backend/lib/emailSender.js
+++ backend/lib/emailSender.js
@@ -5,18 +5,24 @@ export const sendInvoiceEmail = async ({
   name,
   invoicePath,
 }) => {
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: false,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+//   const transporter = nodemailer.createTransport({
+//     host: process.env.SMTP_HOST,
+//     port: Number(process.env.SMTP_PORT),
+//     secure: false,
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//   });
 
-  await transporter.sendMail({
-    from: `"Support" <${process.env.SMTP_USER}>`,
+
+      const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: { user: process.env.FROM, pass: process.env.FROMPASSWORD },
+      });
+      // from: `"Support" <${process.env.SMTP_USER}>`,
+      await transporter.sendMail({
+      from: `"Support" <${process.env.FROM}>`,
     to,
     subject: "Your Invoice",
     html: `<p>Hello ${name},</p>
